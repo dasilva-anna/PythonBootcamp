844645512c8205a382b2e0444dba5cfe4aed6209
Anna da Silva <dasilva.annap@gmail.com>
2022-03-24 19:00:11 -0600

    Add files via upload

---
+++ day_3_treasure_island.py
@@ -0,0 +1,49 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+direction = input("What direction would you like to follow? Left or right?\n")
+direction = direction.lower()
+if direction == 'left':
+  choice2 = input("Good choice! Now we have to cross a lake, would you like to swim or wait for a boat? Type swim or wait\n")
+  choice2 = choice2.lower()
+  # Next step
+  if choice2 == 'wait':
+    door = input("Whew, you survived another level. Now, you have to choose a door to enter. Red, blue, or yellow?\n")
+    door = door.lower()
+    # Choosing a door
+    if door == 'yellow':
+      print("Congrats, you found the treasure!")
+    elif door == 'red':
+      print("That room was on fire. I'm sorry, you died. Try again.")
+    else:
+      print("You were eaten by exotic animals. I'm sorry. Try again.")  
+  else:
+    print("Oh man, you were attacked by trouts and died. Try again.")
+else:
+  print("You fell into a pit of rattlesnake. I'm sorry, you died. Try again.")
+
